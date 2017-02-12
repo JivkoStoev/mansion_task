71c628ac952d8d0f8f3195533e4b960e501249b3
DESKTOP-9KBRH4D\Jivko Stoev-homeoffice <jivko_1990@mail.bg>
2017-02-12 23:21:33 +0200

    form + little design

--- app/scripts/bundle.js
+++ app/scripts/bundle.js
@@ -1,1 +1,20 @@
-console.log('hello');
+'use strict';
+
+
+$(document).ready(function() {
+    console.log('ready');
+    $('#bonus-code-checkbox').click(function() {
+        $('#bonus-code').toggle();
+    })
+
+
+
+    $('#reg-form').validate({
+        rules: {
+            inputFirstName: "required",
+        },
+        messages: {
+            inputFirstName: "Please enter your firstname",
+        }
+    })
+});
